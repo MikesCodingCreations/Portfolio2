7630ee155bee780550a05422540d52e91b4cd5ec
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-05-05 12:59:05 +0000

    changes made to Script 2 file as per JSHINT warnings

--- script2.js
+++ script2.js
@@ -3,21 +3,21 @@ function togglePopup() {
     document.getElementById('popup-1').classList.toggle('active');
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset');
-    const announcer = document.querySelector('.announcer');
+window.addEventListener('DOMContentLoaded', function() {
+    var tiles = Array.from(document.querySelectorAll('.tile'));
+    var playerDisplay = document.querySelector('.display-player');
+    var resetButton = document.querySelector('#reset');
+    var announcer = document.querySelector('.announcer');
 
-    let board = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = 'X';
-    let isGameActive = true;
+    var board = ['', '', '', '', '', '', '', '', ''];
+    var currentPlayer = 'X';
+    var isGameActive = true;
 
-    const PLAYERX_WON = 'PLAYERX_WON';
-    const PLAYERO_WON = 'PLAYERO_WON';
-    const TIE = 'TIE';
+    var PLAYERX_WON = 'PLAYERX_WON';
+    var PLAYERO_WON = 'PLAYERO_WON';
+    var TIE = 'TIE';
 
-    const winningConditions = [
+    var winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -29,12 +29,12 @@ window.addEventListener('DOMContentLoaded', () => {
     ];
 
     function handleResultValidation() {
-        let roundWon = false;
-        for (let i = 0; i <= 7; i++) {
-            const winCondition = winningConditions[i];
-            const a = board[winCondition[0]];
-            const b = board[winCondition[1]];
-            const c = board[winCondition[2]];
+        var roundWon = false;
+        for (var i = 0; i <= 7; i++) {
+            var winCondition = winningConditions[i];
+            var a = board[winCondition[0]];
+            var b = board[winCondition[1]];
+            var c = board[winCondition[2]];
             if (a === '' || b === '' || c === '') {
                 continue;
             }
@@ -54,7 +54,7 @@ window.addEventListener('DOMContentLoaded', () => {
         announce(TIE);
     }
 
-    const resetBoard = () => {
+    var resetBoard = function() {
         board = ['', '', '', '', '', '', '', '', ''];
         isGameActive = true;
         announcer.classList.add('hide');
@@ -68,10 +68,9 @@ window.addEventListener('DOMContentLoaded', () => {
             tile.classList.remove('playerX');
             tile.classList.remove('playerO');
         });
-    }
+    };
 
-
-    const announce = (type) => {
+    var announce = function(type) {
         switch(type){
             case PLAYERO_WON:
                 announcer.innerHTML = 'Player <span class="playerO">O</span> Wins';
@@ -85,7 +84,7 @@ window.addEventListener('DOMContentLoaded', () => {
         announcer.classList.remove('hide');
     };
 
-    const isValidAction = (tile) => {
+    var isValidAction = function(tile) {
         if (tile.innerText === 'X' || tile.innerText === 'O'){
             return false;
         }
@@ -93,18 +92,18 @@ window.addEventListener('DOMContentLoaded', () => {
         return true;
     };
 
-    const updateBoard =  (index) => {
+    var updateBoard =  function(index) {
         board[index] = currentPlayer;
-    }
+    };
 
-    const changePlayer = () => {
+    var changePlayer = function() {
         playerDisplay.classList.remove(`player${currentPlayer}`);
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         playerDisplay.innerText = currentPlayer;
         playerDisplay.classList.add(`player${currentPlayer}`);
-    }
+    };
 
-    const userAction = (tile, index) => {
+    var userAction = function(tile, index) {
         if(isValidAction(tile) && isGameActive) {
             tile.innerText = currentPlayer;
             tile.classList.add(`player${currentPlayer}`);
@@ -112,9 +111,9 @@ window.addEventListener('DOMContentLoaded', () => {
             handleResultValidation();
             changePlayer();
         }
-    }
+    };
     
-    tiles.forEach( (tile, index) => {
+    tiles.forEach( function(tile, index) {
         tile.addEventListener('click', () => userAction(tile, index));
     });
 
